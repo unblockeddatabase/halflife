1fd1d0896dca5be01f4e316a1709558e75826a95
Emupedia <admin@emupedia.net>
2020-06-08 04:27:48 +0300

    More work

--- js/main.js
+++ js/main.js
@@ -19,14 +19,14 @@
 	requirejs.config({
 		waitSeconds: 300,
 		paths: {
-			browserfs: ['/beta/emuos/js/libraries/browserfs-1.4.3.min', 'https://emupedia.net/beta/emuos/js/libraries/browserfs-1.4.3.min', 'https://emuos.net/beta/emuos/js/libraries/browserfs-1.4.3.min'],
-			dropbox: ['/beta/emuos/js/libraries/dropbox-4.0.30.min', 'https://emupedia.net/beta/emuos/js/libraries/dropbox-4.0.30.min', 'https://emuos.net/beta/emuos/js/libraries/dropbox-4.0.30.min'],
-			simplestorage: ['/beta/emuos/js/libraries/simplestorage-0.2.1.min', 'https://emupedia.net/beta/emuos/js/libraries/simplestorage-0.2.1.min', 'https://emuos.net/beta/emuos/js/libraries/simplestorage-0.2.1.min'],
-			es6promise: ['/beta/emuos/js/libraries/polyfill-es6-promise-auto-4.2.8.min', 'https://emupedia.net/beta/emuos/js/libraries/polyfill-es6-promise-auto-4.2.8.min', 'https://emuos.net/beta/emuos/js/libraries/polyfill-es6-promise-auto-4.2.8.min'],
-			es6fetch: ['/beta/emuos/js/libraries/polyfill-es6-fetch-3.0.0', 'https://emupedia.net/beta/emuos/js/libraries/polyfill-es6-fetch-3.0.0', 'https://emuos.net/beta/emuos/js/libraries/polyfill-es6-fetch-3.0.0'],
-			jquery: ['/beta/emuos/js/libraries/jquery-3.5.0.min', 'https://emupedia.net/beta/emuos/js/libraries/jquery-3.5.0.min', 'https://emuos.net/beta/emuos/js/libraries/jquery-3.5.0.min'],
-			json: ['/beta/emuos/js/libraries/requirejs-json-1.0.3', 'https://emupedia.net/beta/emuos/js/libraries/requirejs-json-1.0.3', 'https://emuos.net/beta/emuos/js/libraries/requirejs-json-1.0.3'],
-			text: ['/beta/emuos/js/libraries/requirejs-text-2.0.15', 'https://emupedia.net/beta/emuos/js/libraries/requirejs-text-2.0.15', 'https://emuos.net/beta/emuos/js/libraries/requirejs-text-2.0.15']
+			browserfs: 'libraries/browserfs-1.4.3.min',
+			dropbox: 'libraries/dropbox-4.0.30.min',
+			simplestorage: 'libraries/simplestorage-0.2.1.min',
+			es6promise: 'libraries/polyfill-es6-promise-auto-4.2.8.min',
+			es6fetch: 'libraries/polyfill-es6-fetch-3.0.0',
+			jquery: 'libraries/jquery-3.5.0.min',
+			json: 'libraries/requirejs-json-1.0.3',
+			text: 'libraries/requirejs-text-2.0.15'
 		},
 		shim: {
 			browserfs: {
